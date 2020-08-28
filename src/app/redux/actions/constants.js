91e9dbfe84c93bbec63f219b587385bd7ba220c5
Maksym7415 <popovmaksim7415@gmail.com>
2020-08-28 10:00:52 +0300

    fixed endpoint api

--- src/app/redux/actions/constants.js
+++ src/app/redux/actions/constants.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const myAxios = (url, data) => () => axios({
 	method: url.method,
-	url: `http://ec2-18-219-225-191.us-east-2.compute.amazonaws.com:8080/${url.url}`,
+	url: `ec2-18-219-225-191.us-east-2.compute.amazonaws.com:8080/${url.url}`,
 	headers: localStorage.authToken ? {
 		'Content-Type': 'application/json',
 		Authorization: `Bearer ${localStorage.authToken}`
